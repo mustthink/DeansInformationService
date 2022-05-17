0f1fea27cbadbe5569a0e1e75d2ba06a53a9a127
mustthink <007kolr007@gmail.com>
2022-05-17 13:25:52 +0300

    Try docker mysql

--- docker/setup.sql
+++ docker/setup.sql
@@ -1,9 +1,7 @@
-CREATE DATABASE acc_db;
-
-USE accountdb;
+USE dis_db;
 
 CREATE TABLE accounts (
-                          id INT auto_increment PRIMARY KEY CHECK (id > 0),
+                          id INT PRIMARY KEY CHECK (id > 0),
                           login VARCHAR (30) NOT NULL,
                           password VARCHAR(30) NOT NULL DEFAULT 123456,
                           type INT NOT NULL DEFAULT 3 CHECK (type > 0 AND type < 4)
